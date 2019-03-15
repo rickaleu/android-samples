66a4b8200bfb1309d4d840e848119b073d929877
rickale <ti.ricardo.sousa@gmail.com>
2019-03-15 15:00:59 -0300

    voltando com o exemplo simples de fragment

--- app/src/main/java/br/com/ricardo/androidsamples/MainActivity.java
+++ app/src/main/java/br/com/ricardo/androidsamples/MainActivity.java
@@ -1,13 +1,12 @@
 package br.com.ricardo.androidsamples;
-
-import android.content.Intent;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,22 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
         //Abre uma transação e adiciona o fragment
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.fragment_content, new MainFragment());
+        fragmentTransaction.add(R.id.fragment_container, new MainFragment());
         fragmentTransaction.commit();
-
-        //Substitui um fragment
-        //FragmentTransaction fragmentTransaction1 = fragmentManager.beginTransaction();
-        //fragmentTransaction1.replace(R.id.fragment_content, new MainFragment());
-        //fragmentTransaction1.commit();
-
-        //Remove um fragment
-        //Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_content);
-        //FragmentTransaction fragmentTransaction2 = fragmentManager.beginTransaction();
-        //fragmentTransaction2.remove(fragment);
-        //fragmentTransaction2.commit();
-
-
-
 
     }
 }
--- app/src/main/java/br/com/ricardo/androidsamples/MainFragment.java
+++ app/src/main/java/br/com/ricardo/androidsamples/MainFragment.java
@@ -7,20 +7,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+
 
 public class MainFragment extends Fragment {
-
-    public TextView textFragment;
-    public Button buttonFragment;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.main_fragment, container, false);
-
 
         return v;
     }
